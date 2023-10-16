4507fe81629de6dd548ab47d3031669bdf23d65c
Jaswanth <pjaswanthkumar270602@gmail.com>
2023-10-16 23:53:09 +0530

    updated api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,7 +6,7 @@ export const YOUTUBE_VIDEOS_API=
 "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY;
 console.log(YOUTUBE_VIDEOS_API);
 export const YOUTUBE_SUGG_API=
-"http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+"https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 // export const YOUTUBE_SUGG_API=
 // "http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=";
